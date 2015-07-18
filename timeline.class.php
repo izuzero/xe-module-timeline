1f640baeebed8cec72dd60be0a54854da7e1c397
Eunsoo Lee <izuzero@gmail.com>
2015-07-18 20:30:26 +0900

    #34 Duplicate Index fixed

--- timeline.class.php
+++ timeline.class.php
@@ -9,7 +9,7 @@
 class timeline extends ModuleObject
 {
 	// table, column, type, size, default, notnull
-	private $columns = array(
+	protected static $columns = array(
 		array( 'timeline_registered_info', 'notice',             'char',    1,    'N',   TRUE  ),
 		array( 'timeline_registered_info', 'replace',            'char',    1,    'N',   TRUE  ),
 		array( 'timeline_registered_info', 'write',              'char',    1,    'N',   TRUE  ),
@@ -18,18 +18,22 @@ class timeline extends ModuleObject
 		array( 'timeline_registered_info', 'standard_date',      'date',    NULL, NULL,  FALSE ),
 		array( 'timeline_registered_info', 'limit_date',         'date',    NULL, NULL,  FALSE ),
 		array( 'timeline_registered_info', 'auto_renewal',       'char',    1,    'N',   TRUE  ),
-		array( 'timeline_attach_info',     'priority',           'number',  11,   '1',   TRUE  )
+		array( 'timeline_attach_info',     'priority',           'number',  11,   '1',   FALSE )
+	);
+
+	// table, index, unique
+	protected static $deletedIndexes = array(
+		array( 'timeline_attach_info', 'idx_priority', TRUE )
 	);
 
 	// table, index, column, unique
-	private $indexes = array(
+	protected static $indexes = array(
 		array( 'timeline_registered_info', 'idx_popular_count', array('popular_count'), FALSE ),
 		array( 'timeline_registered_info', 'idx_standard_date', array('standard_date'), FALSE ),
-		array( 'timeline_registered_info', 'idx_limit_date',    array('limit_date'),    FALSE ),
-		array( 'timeline_attach_info',     'idx_priority',      array('priority'),      FALSE )
+		array( 'timeline_registered_info', 'idx_limit_date',    array('limit_date'),    FALSE )
 	);
 
-	private $triggers = array(
+	protected static $triggers = array(
 		array( 'moduleHandler.init', 'timeline', 'controller', '_setTimelineInfo',          'after'  ),
 		array( 'moduleHandler.init', 'timeline', 'controller', '_replaceMid',               'before' ),
 		array( 'moduleHandler.init', 'timeline', 'controller', '_replaceModuleInfo',        'after'  ),
@@ -43,7 +47,8 @@ class timeline extends ModuleObject
 	function moduleInstall()
 	{
 		$oModuleController = getController('module');
-		foreach ($this->triggers as $trigger)
+
+		foreach (self::$triggers as $trigger)
 		{
 			$oModuleController->insertTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
 		}
@@ -54,7 +59,8 @@ class timeline extends ModuleObject
 	function moduleUninstall()
 	{
 		$oModuleController = getController('module');
-		foreach ($this->triggers as $trigger)
+
+		foreach (self::$triggers as $trigger)
 		{
 			$oModuleController->deleteTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
 		}
@@ -66,21 +72,29 @@ class timeline extends ModuleObject
 	{
 		$oDB = DB::getInstance();
 		$oModuleModel = getModel('module');
-		foreach ($this->columns as $column)
+
+		foreach (self::$columns as $column)
 		{
 			if (!$oDB->isColumnExists($column[0], $column[1]))
 			{
 				return TRUE;
 			}
 		}
-		foreach ($this->indexes as $index)
+		foreach (self::$deletedIndexes as $index)
+		{
+			if ($oDB->isIndexExists($index[0], $index[1]))
+			{
+				return TRUE;
+			}
+		}
+		foreach (self::$indexes as $index)
 		{
 			if (!$oDB->isIndexExists($index[0], $index[1]))
 			{
 				return TRUE;
 			}
 		}
-		foreach ($this->triggers as $trigger)
+		foreach (self::$triggers as $trigger)
 		{
 			if (!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
 			{
@@ -96,21 +110,29 @@ class timeline extends ModuleObject
 		$oDB = DB::getInstance();
 		$oModuleModel = getModel('module');
 		$oModuleController = getController('module');
-		foreach ($this->columns as $column)
+
+		foreach (self::$columns as $column)
 		{
 			if (!$oDB->isColumnExists($column[0], $column[1]))
 			{
 				$oDB->addColumn($column[0], $column[1], $column[2], $column[3], $column[4], $column[5]);
 			}
 		}
-		foreach ($this->indexes as $index)
+		foreach (self::$deletedIndexes as $index)
+		{
+			if ($oDB->isIndexExists($index[0], $index[1]))
+			{
+				$oDB->dropIndex($index[0], $index[1], $index[2]);
+			}
+		}
+		foreach (self::$indexes as $index)
 		{
 			if (!$oDB->isIndexExists($index[0], $index[1]))
 			{
 				$oDB->addIndex($index[0], $index[1], $index[2], $index[3]);
 			}
 		}
-		foreach ($this->triggers as $trigger)
+		foreach (self::$triggers as $trigger)
 		{
 			if (!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
 			{
